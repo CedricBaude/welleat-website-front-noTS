6b50ef5f3e8559d337380343404887ea788891c1
Cedric Baude <baude.cedric.perso@gmail.com>
2022-11-26 23:51:53 +0100

    update admin dahsboard header css

--- src/components/admin/Header.js
+++ src/components/admin/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { accountService } from '../../_services/account.service';
+
 const Header = () => {
     let navigate = useNavigate();
     const logOut = () => {
         accountService.logOut();
         navigate('/');
     }
+    let activeStyle = {
+        backgroundColor: "#FFC959",
+    };
     return (
         <header className='header-dashboard'>
             <div className="user-desc">
@@ -20,18 +24,24 @@ const Header = () => {
                     <span className='ita'>Manager</span>
                 </div>
             </div>
-            <nav>
-                <ul className='user-desc-ul'>
-                    <li><Link to="/admin/dashboard">DASHBOARD</Link></li>
-                    <li><Link to="/admin/review">AVIS UTILISATEURS</Link></li>
-                    <li><Link to="/admin/contact">CONTACT</Link></li>
-                    <li><Link to="/admin/simulation">SIMULATION DE GAINS</Link></li>
-                    <li><Link to="/admin/newsletter">NEWSLETTER</Link></li>
-                    <li><Link to="/admin/story">STORY</Link></li>
-                    <li><Link to="/admin/media">MEDIA</Link></li>
-                </ul>
-            </nav>
-            <button className='logout' onClick={logOut}>DECONNEXION</button>
+            <div className="header-nav">
+                <nav>
+                    <ul className='user-desc-ul'>
+                        <li><NavLink to="/admin/dashboard" style={({ isActive }) => isActive ? activeStyle : undefined}>DASHBOARD</NavLink></li>
+                        <li><NavLink to="/admin/review" style={({ isActive }) => isActive ? activeStyle : undefined}>AVIS UTILISATEURS</NavLink></li>
+                        <li><NavLink to="/admin/contact" style={({ isActive }) => isActive ? activeStyle : undefined}>CONTACT</NavLink></li>
+                        <li><NavLink to="/admin/simulation" style={({ isActive }) => isActive ? activeStyle : undefined}>SIMULATION DE GAINS</NavLink></li>
+                        <li><NavLink to="/admin/newsletter" style={({ isActive }) => isActive ? activeStyle : undefined}>NEWSLETTER</NavLink></li>
+                        <li><NavLink to="/admin/story" style={({ isActive }) => isActive ? activeStyle : undefined}>STORY</NavLink></li>
+                        <li><NavLink to="/admin/media" style={({ isActive }) => isActive ? activeStyle : undefined}>MEDIA</NavLink></li>
+                    </ul>
+                </nav>
+                <div className="logOutButton">
+                    <button className='logout' onClick={logOut}>DECONNEXION</button>
+                </div>
+
+            </div>
+
         </header>
     );
 };
