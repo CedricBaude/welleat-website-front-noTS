d07ce6169f023a218523077cd51f86de8861eb59
Cedric Baude <baude.cedric.perso@gmail.com>
2023-01-19 11:43:00 +0100

    start appli acteur pages

--- src/pages/Public/AppliActeur.js
+++ src/pages/Public/AppliActeur.js
@@ -1,12 +1,43 @@
 import React from 'react';
-
+import chefLogo from './../../assets/img/logo/professions-man-chef-3.png';
 import './style.css';
 
 const AppliActeur = () => {
     return (
-        <div>
-            <h2>Hello, Appli acteur</h2>
-        </div>
+        <>
+
+            <section className='header-appli'>
+                <div className='logo-appli-acteur'>
+                    <img className='chefLogo' src={chefLogo} alt="" />
+                </div>
+                <div>
+                    <h2 className='appli-acteur-title'>Nos outils pour les acteurs de la food </h2>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+
+        </>
     );
 };
 
