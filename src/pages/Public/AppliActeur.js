b8222d322f545fcf301e5edc9bcac78438608317
Cedric Baude <baude.cedric.perso@gmail.com>
2023-01-19 22:13:30 +0100

    add global css

--- src/pages/Public/AppliActeur.js
+++ src/pages/Public/AppliActeur.js
@@ -4,40 +4,53 @@ import './style.css';
 
 const AppliActeur = () => {
     return (
-        <>
+        <div className='appli-acteur'>
 
-            <section className='header-appli'>
+            <section className='header-appli-acteur'>
                 <div className='logo-appli-acteur'>
                     <img className='chefLogo' src={chefLogo} alt="" />
                 </div>
                 <div>
-                    <h2 className='appli-acteur-title'>Nos outils pour les acteurs de la food </h2>
+                    <h2>Nos outils pour les acteurs <br /> de la food </h2>
                 </div>
                 <div>
-
+                    <h3>“Adapter la méthode industrielle à une production de plus petite échelle en la rendant
+                        <br /> plus humaine et plus saine, tout en ne produisant que ce vous allez vendre !”</h3>
                 </div>
                 <div>
-
+                    <h4>900 000 employés utilisent Welleat au quotidien</h4>
                 </div>
 
+            </section >
+
+            <section className='content'>
+                <div className="logo-appli-acteur-carrousel">
+
+                </div>
             </section>
+
             <section>
-
+                <div>
+                    <h2 className='appli-acteur-title'>Ensemble promouvons la <br /> production culinaire</h2>
+                </div>
+                <div>
+                    <h3>Produisons des produits sains pour vos clients et la planéte </h3>
+                </div>
             </section>
+
             <section>
-
+                <h2 className='appli-acteur-title'>Structurer et digitaliser </h2>
             </section>
+
             <section>
-
+                <h2 className='appli-acteur-title'>Optimiser / Anticiper / Adapter </h2>
             </section>
+
             <section>
-
-            </section>
-            <section>
-
+                <h2 className='appli-acteur-title'>Partager et rasurrer </h2>
             </section>
 
-        </>
+        </div>
     );
 };
 
