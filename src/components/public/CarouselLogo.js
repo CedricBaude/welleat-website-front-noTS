757109c4fb3462a84be4a31b0fe2dfb5c595fa90
Cedric Baude <baude.cedric.perso@gmail.com>
2023-02-07 14:54:53 +0100

    add logo user carousel

--- src/components/public/CarouselLogo.js
+++ src/components/public/CarouselLogo.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const CarouselLogo = (props) => {
     return (
-        <div className="users-logo-picture">
-            <div className="users-logo-picture" key={props.id_prod}>
-                <img className='users-logo-picture-img' alt='' src={require('../../assets/img/' + props.url + '')}>
-                </img>
+        <div className='users-logo'>
+            <div className="users-logo-picture">
+                <div className="users-logo-picture" key={props.id_logo}>
+                    <img className='users-logo-picture-img' alt='' src={require('../../assets/img/' + props.url + '')}>
+                    </img>
+                </div>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/public/Use.js
+++ src/components/public/Use.js
@@ -33,14 +33,14 @@ const Use = () => {
     if (!post) return null;
     // const product = productData.map((item) => <Product name={item.name} url={item.imageurl} price={item.price} description={item.description} />);
     // const product2 = postLogo?.data.map((data) => <Product2 id_prod={data?.id} url={data?.lien} />);
-    const carouselLogo = (postLogo?.data || []).map((data) => <CarouselLogo id_prod={data?.id} url={data?.lien} />);
+    const carouselLogo = (postLogo?.data || []).map((data) => <CarouselLogo id_logo={data?.id} url={data?.lien} />);
 
     return (
         <section className='sub-content use' id='returns'>
             <h3 className='sub-title-2'>Ils utilisent Welleat<span className='dot'>.</span> </h3>
-            <div className='users-logo'>
 
-                {/* {
+
+            {/* {
                     postLogo?.data.map((data, index) => (
                         <div className="users-logo-picture" key={data.id}>
                             <img className='users-logo-picture-img' alt='' src={require('../../assets/img/' + postLogo.data[index].lien + '')}>
@@ -50,14 +50,14 @@ const Use = () => {
                 } */}
 
 
-                {/* <div className="users-logo-picture"><img></img> </div>
+            {/* <div className="users-logo-picture"><img></img> </div>
                 <div className="users-logo-picture"><img src="../../assets/img/Photo_Test.png"></img></div>
                 <div className="users-logo-picture"><img src="frontend\welleat-website-front-noTS\src\assets\img\Photo_Test.png"></img></div>
                 <div className="users-logo-picture"></div> */}
+            <div className="users-logo-container">
+                <Carousel responsive={responsive} showDots={false} autoPlay={true} autoPlaySpeed={2000} infinite={true} arrows={false}>{carouselLogo}</Carousel>
+                {/* <Carousel responsive={responsive} showDots={true} autoPlay={true} autoPlaySpeed={2000} infinite={true}>{product2}</Carousel> */}
             </div>
-            <Carousel responsive={responsive} showDots={false} autoPlay={true} autoPlaySpeed={2000} infinite={true}>{carouselLogo}</Carousel>
-            {/* <Carousel responsive={responsive} showDots={true} autoPlay={true} autoPlaySpeed={2000} infinite={true}>{product2}</Carousel> */}
-
 
             {/* {product2} */}
             <div className="users-testi">
