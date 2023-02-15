04be8b88ab03b9c3ebaeba2e88fd4fccd2dc842f
Cedric Baude <baude.cedric.perso@gmail.com>
2023-02-15 17:11:47 +0100

    global update

--- src/pages/Public/AppliActeur.js
+++ src/pages/Public/AppliActeur.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import axios from "axios";
 import chefLogo from './../../assets/img/logo/professions-man-chef-3.png';
 import './style.css';
 
+import Carousel from 'react-multi-carousel';
+import CarouselLogo from './../../components/public/CarouselLogo';
+import { responsive } from './../../components/public/CarouselData';
+
+const baseURLLogo = "https://testrender-6iwm.onrender.com/logo";
+
 const AppliActeur = () => {
+
+    const [postLogo, setPostLogo] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURLLogo).then((response) => {
+            // console.log(response.data);
+            setPostLogo(response.data);
+            console.log(response.data);
+        });
+    }, []);
+
+    const carouselLogo = (postLogo?.data || []).map((data) => <CarouselLogo id_logo={data?.id} url={data?.lien} />);
+
     return (
         <div className='appli-acteur'>
 
@@ -25,32 +45,33 @@ const AppliActeur = () => {
 
             <section className='appli-acteur-content'>
                 <div className="logo-appli-acteur-carrousel">
+                    <div className="logo-appli-acteur-carrousel-content">
+                        <Carousel responsive={responsive} showDots={false} autoPlay={true} autoPlaySpeed={2000} infinite={true} arrows={false}>{carouselLogo}</Carousel>
+                    </div>
 
                 </div>
             </section>
 
-            <section>
-                <div>
-                    <h2 className='appli-acteur-title'>Ensemble promouvons la <br /> production culinaire</h2>
-                    <div>
+            <section className='appli-acteur-content'>
 
-                    </div>
-                </div>
+                <h2 className='appli-acteur-title'>Ensemble promouvons la <br /> production culinaire</h2>
+                <p>Produisons des produits sains pour vos clients et la planéte </p>
+
                 <div>
                     <h3>Produisons des produits sains pour vos clients et la planéte </h3>
                 </div>
             </section>
 
-            <section>
-                <h2 className='appli-acteur-title'>Structurer et digitaliser </h2>
+            <section className='appli-acteur-content'>
+                <h2 className='appli-acteur-subtitle'>Structurer et digitaliser </h2>
             </section>
 
-            <section>
-                <h2 className='appli-acteur-title'>Optimiser / Anticiper / Adapter </h2>
+            <section className='appli-acteur-content'>
+                <h2 className='appli-acteur-subtitle'>Optimiser / Anticiper / Adapter </h2>
             </section>
 
-            <section>
-                <h2 className='appli-acteur-title'>Partager et rasurrer </h2>
+            <section className='appli-acteur-content'>
+                <h2 className='appli-acteur-subtitle'>Partager et rasurrer </h2>
             </section>
 
         </div>
